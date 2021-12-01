514882340553694eb2554ef1aa4651cc65711ae2
k-yutok <ciyt11058@g.nihon-u.ac.jp>
2021-12-01 23:38:10 +0900

    first

---
+++ PortfolioApp/PortfolioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioAppApp.swift
+//  PortfolioApp
+//
+//  Created by yuutoku kouno on 2021/12/01.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
